d8a505497e32f1c38c258ae63f13ef6412a2b00e
u210709016 <ridvanbarisozden@posta.mu.edu.tr>
2024-12-29 22:16:00 +0300

    joining channel

--- app/src/main/java/msku/ceng/madlab/roxid/database/Users.java
+++ app/src/main/java/msku/ceng/madlab/roxid/database/Users.java
@@ -19,6 +19,14 @@ public class Users {
         this.userPicture = userPicture;
     }
 
+    public Users(String username, String userPicture) {
+        this.username = username;
+        this.userPicture = userPicture;
+    }
+
+    public String getUsername() {
+        return username;
+    }
     /*
     public int getUserId() {
         return userId;
@@ -56,9 +64,7 @@ public class Users {
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
+
 
     public void setUsername(String username) {
         this.username = username;
---
+++ app/src/main/java/msku/ceng/madlab/roxid/voiceChannelUserHold/MyVoiceChannelUserHolderRecyclerViewAdapter.java
@@ -0,0 +1,71 @@
+package msku.ceng.madlab.roxid.voiceChannelUserHold;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.firestore.auth.User;
+
+import msku.ceng.madlab.roxid.R;
+import msku.ceng.madlab.roxid.database.Users;
+import msku.ceng.madlab.roxid.databinding.FragmentVoiceChannelUserHolderItemBinding;
+
+import java.util.List;
+
+
+public class MyVoiceChannelUserHolderRecyclerViewAdapter extends RecyclerView.Adapter<MyVoiceChannelUserHolderRecyclerViewAdapter.ViewHolder> {
+
+    private  List<Users> mValues;
+
+    public MyVoiceChannelUserHolderRecyclerViewAdapter(List<Users> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_voice_channel_user_holder_item,parent,false);
+
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.profileImage.setImageResource(R.mipmap.ic_launcher);
+        holder.mContentView.setText(mValues.get(position).getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public void updateData(List<Users> newData) {
+        this.mValues = newData; // Replace the old data
+        notifyItemInserted(0);  // Refresh the RecyclerView
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final ShapeableImageView profileImage;
+        public final TextView mContentView;
+        public final View mView;
+        public Users userItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mContentView = view.findViewById(R.id.username);
+            profileImage = view.findViewById(R.id.user_profile_image);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
