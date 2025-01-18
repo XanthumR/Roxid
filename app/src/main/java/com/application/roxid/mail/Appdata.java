a17e6ffa38475e8f13adf6a5ebe59428be373375
Rıdvan Barış Özden <159203563+XanthumR@users.noreply.github.com>
2025-01-18 19:07:56 +0300

    Create Appdata.java

---
+++ app/src/main/java/com/application/roxid/mail/Appdata.java
@@ -0,0 +1,10 @@
+package com.application.roxid.mail;
+
+public class Appdata {
+
+    public static  final String Sender_Email = "gmail you want to send the confirmation code";
+    public static  final String Sender_Email_Password = "get your app password from google manage account";
+    public static  final String Gmail_Host = "smtp.gmail.com";
+
+
+}
